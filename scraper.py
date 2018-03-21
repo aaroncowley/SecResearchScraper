13d0d334bad8c15bb7098208e597a64b86b1bd2d
Aaron Cowley <acow777@ad.it.et.byu.edu>
2018-03-21 11:00:49 -0600

    added comments

--- scraper.py
+++ scraper.py
@@ -2,6 +2,28 @@
 import time
 from selenium import webdriver
 
+# ---------------------------------------- #
+'''
+
+FUNCTIONALITY GOALS
+
+Scrape... 
+    Authors
+    Title
+    Journal
+    Publication year
+    Volume
+    Issue
+    Pages
+    DOI
+From...
+    TODO: 1 - Initial query results
+    TODO: 2 - Citations FROM search results
+    TODO: 3 - Citations TO THE search results
+
+
+'''
+# --------------------------------------- #
 driver = webdriver.Chrome('chromedriver.exe')
 driver.get('http://apps.webofknowledge.com/summary.do?product=WOS&parentProduct=WOS&search_mode=GeneralSearch&qid=1&SID=5AMkc7rK32QEqeo4cm7&page=1&action=changePageSize&pageSize=1000');
 time.sleep(5)
@@ -10,7 +32,9 @@ resultElement = driver.find_element(by='id', value='hitCount.top')
 resultNum = resultElement.text
 print(resultNum)
 for i in range(resultNum):
-    
-    
+'''
+do logic on each page here
+
+'''
 
 driver.quit()
